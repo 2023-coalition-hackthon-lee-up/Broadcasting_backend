64b5c8dd78198427595b99671697e241b3bc8faa
Overraddit <kenwooshim@gmail.com>
2023-03-19 18:10:47 +0900

    Feat: broadcasting

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,10 +1,15 @@
 import {
+  ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import axios from 'axios';
+import { Server, Socket } from 'socket.io';
 
 // 이 설정들이 뭘하는건지, 애초에 무슨 레포를 보고 이것들을 찾을 수 있는지 전혀 모르겠다.
 @WebSocketGateway(8000, {
@@ -15,14 +20,55 @@ import { Server } from 'socket.io';
     credentials: true,
   },
 })
-
-export class EventsGateway {
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('events')
   async handleEvent(@MessageBody() data) {
-    console.log(data);
+    //console.log('handleEvent: ', data);
     //return data;
+  }
+
+  afterInit() {
+    this.server.on('welcome', (room) => {
+      console.log('welcome event');
+    });
+    console.log('afterInit');
+  }
+
+  // 소켓이 연결되면 실행
+  async handleConnection(@ConnectedSocket() socket: Socket) {
+    const playlistId = process.env.PLAYLIST_ID;
+    const YOUTUBE_KEY = process.env.YOUTUBE_KEY;
+
+    // 1. youtube playlist를 가져온다.
+    let part = "id, snippet, contentDetails, status";
+
+    const res = await axios.get(
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=${part}&maxResults=6&playlistId=${playlistId}&key=${YOUTUBE_KEY}`,
+    );
+    const playlist = res.data.items;
+
+    part = 'contentDetails';
+    let videoId = '';
+    playlist.forEach((element) => {
+      videoId += `${element.snippet.resourceId.videoId},`;
+    });
+    //console.log('videoIdList: ', videoId);
+    const Videores = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${YOUTUBE_KEY}&part=contentDetails`
+    );
+    // 2. playlist를 client에 전달한다.
+    // 왜인지 모르겠지만 Videores를 직접 전달하면 함수최대스택을 초과해 터져버린다.
+    const metaList = Videores.data.items;
+    socket.emit('playlist', playlist, metaList);
+  }
+
+  // 소켓 연결이 끊기면 실행
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    console.log('handleDisconnect');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 import { EventsModule } from './events/events.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(EventsModule);
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(3000);
