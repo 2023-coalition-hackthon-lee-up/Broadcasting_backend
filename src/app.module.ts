0e59c76f243edcae16570f31456ad07e7742467d
Overraddit <kenwooshim@gmail.com>
2023-03-19 18:16:57 +0900

    FIX: modify extra file and package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events/events.gateway';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
