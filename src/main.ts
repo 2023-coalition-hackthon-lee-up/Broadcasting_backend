3a62ef39b580d61be068b20ad62fbd9bba25e68a
Overraddit <kenwooshim@gmail.com>
2023-04-02 20:19:44 +0900

    Feat: add CORS availabler list

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,23 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 import { EventsModule } from './events/events.module';
 import { config } from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
+
+  const corsOptions: CorsOptions = {
+    origin: [
+      'http://localhost:3000',
+      'https://2023-coalition-hackthon-lee-up.github.io/Broadcasting_frontend/',
+    ], // set the allowed origin(s)
+    //origin: ['*'], // set the allowed origin(s)
+    optionsSuccessStatus: 200, // set the status code for successful CORS preflight requests
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(8080);
 }
 bootstrap();
